e9031187df2441a919a04dca8d07b1cc83283acf
Sijan Shrestha <slimshaddy.d@gmail.com>
2019-03-01 23:02:47 +0545

    Add files via upload

--- Decipher.py
+++ Decipher.py
@@ -1,14 +1,31 @@
 from Encipher import enciphered_text
 import collections
-print("Enciphered Text:\t"+enciphered_text)
-deciphered_text = list(enciphered_text)
+# Removing blankspace from the encrypted file from Encipher.py ensuring it's not the most repeated character
 enciphered_text1 = enciphered_text.replace(' ', '')
+
+# m is the most repeated character and n is the number of m in the enciphered_text
 m, n = collections.Counter(enciphered_text1).most_common(1)[0]
-key = ord(m)-ord('e')
-for i in range(len(deciphered_text)):
-    if deciphered_text[i].isalpha():
-        num = (ord(deciphered_text[i])-96)-5
-        if num <= 0:
-            num = num + 26
-        deciphered_text[i] = chr(num+96)
-print("Deciphered Text:\t"+''.join(deciphered_text))
+
+
+def decipher(char):
+    deciphered_text = list(enciphered_text)
+    # Finding key considering char as the most repeated character
+    key = ord(m)-ord(char)
+    for i in range(len(deciphered_text)):
+        # Only substituting the alphabets
+        if deciphered_text[i].isalpha():
+            num = ((ord(deciphered_text[i])-97)-key) % 26
+            deciphered_text[i] = chr(num+97)
+    return ''.join(deciphered_text)
+# "etaoin" is considered most repeated 6 letters in English language
+# So observing the results considering them the most repeated character  for each individual
+# Six results should be observed to find the valid output, or we can check the number of valid words in each output
+# considering the output with most valid words the correct one.
+
+
+print("Deciphered Text considering letter 'e':\t"+decipher('e')+"\n")
+print("Deciphered Text considering letter 't':\t"+decipher('t')+"\n")
+print("Deciphered Text considering letter 'a':\t"+decipher('a')+"\n")
+print("Deciphered Text considering letter 'o':\t"+decipher('o')+"\n")
+print("Deciphered Text considering letter 'i':\t"+decipher('i')+"\n")
+print("Deciphered Text considering letter 'n':\t"+decipher('n'))
--- Encipher.py
+++ Encipher.py
@@ -1,11 +1,12 @@
-f = open('Original_text.txt', 'r')
+f = open('Original.txt', 'r')
 original_text = list(str.lower(f.read()))
-#print("Original Text:\t"+''.join(original_text))
+print("Original Text:\t"+''.join(original_text)+"\n")
+# Enciphering the original text using Caesar Cipher with key=5
+key = 4
 for i in range(len(original_text)):
+    # Only substituting the alphabets
     if original_text[i].isalpha():
-        num = ((ord(original_text[i])-96)+5)%26
-        if num ==0:
-            num = num +26
-        original_text[i] = chr(num+96)
+        num = ((ord(original_text[i])-97)+key) % 26
+        original_text[i] = chr(num+97)
 enciphered_text = ''.join(original_text)
-print("Enciphered Text:\t"+enciphered_text)
+print("Enciphered Text:\t"+enciphered_text+"\n")
